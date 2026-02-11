63f8c272fc753d8aa158770715688c00b78deb37
Boopathy133 <maniboopathy133@gmail.com>
2026-02-11 12:40:16 +0530

    For hosting update

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,20 @@ const slackAlertJobApplication = require("./routes/slackAlertJobApplication");
 const app = express();
 
 // ✅ Enable CORS for Vite frontend
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST"],
+// }));
+
+
 app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST"],
+  origin: [
+    "http://localhost:5173",
+    "https://azhizen-portfolio-main.vercel.app/"
+  ],
+  methods: ["GET", "POST"]
 }));
+
 
 // Parse JSON
 app.use(express.json());
@@ -16,7 +26,7 @@ app.use(express.json());
 // Routes
 app.use("/api", slackAlertJobApplication);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
