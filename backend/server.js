f35c2d613e66460cd84dbb78840ad6b596dcfe16
Boopathy133 <maniboopathy133@gmail.com>
2026-02-11 13:59:19 +0530

    CORS update

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,33 @@ const app = express();
 // }));
 
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://azhizen-portfolio-main.vercel.app/"
-  ],
-  methods: ["GET", "POST"]
-}));
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://azhizen-portfolio-main.vercel.app/",
+//     "https://azhizen-portfolio-main.vercel.app",
+//     "https://www.azhizen.com/"
+//   ],
+//   methods: ["GET", "POST"]
+// }));
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://azhizen-portfolio-main.vercel.app",
+  "https://www.azhizen.com/"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
+}));
 
 // Parse JSON
 app.use(express.json());
